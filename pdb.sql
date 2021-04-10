55ef9c0c45c59c874896c65790faeb9124606568
oravr_201 <info@oravr.in>
2021-04-10 09:07:12 +0530

    commit

--- pdb.sql
+++
@@ -1,4 +0,0 @@
-COLUMN name NEW_VALUE name NOPRINT;
-select  name from v$containers where con_id=&1;
-alter session set container=&name;
-@login
--- shut.sh
+++
@@ -1,7 +0,0 @@
-
-
-$ORACLE_HOME/bin/./lsnrctl stop 
-$ORACLE_HOME/bin/./sqlplus / as sysdba << EOF
-shut immediate;
-exit;
-EOF
